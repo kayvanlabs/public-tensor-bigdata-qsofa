81d3fd44248ee1dd14487120f62bb67d5982d559
Olivia Alge (umich) <oialge@umich.edu>
2023-06-26 12:08:57 -0400

    Move appendEhrData

--- runSOFApipeline.m
+++ runSOFApipeline.m
@@ -88,6 +88,7 @@ function runSOFApipeline(gapDuration, signalDuration, nWindows, discrepDuration)
     [rawEcgTable, rawAbpTable] = prepareRawSignals(configFile, signalsInfo, signalDuration);
     
     %% Get filtered Signals
+    addpath('QRS_detection\')
     [~, filteredEcgTable, ~] = getQrsAndFilteredEcg(rawEcgTable);
     filteredAbpTable = getFilteredArt(rawAbpTable);
     eFile = fullfile(configFile.(lower(outcomeName)), ...
@@ -96,9 +97,12 @@ function runSOFApipeline(gapDuration, signalDuration, nWindows, discrepDuration)
     filteredAbpWithEhr = appendEhrData(filteredAbpTable, tempEhr.features);
     [filteredEcgWithEhr, ehrNames] = appendEhrData(filteredEcgTable, tempEhr.features);
     
+    save('filteredEcgWithEhr.mat', 'filteredEcgWithEhr')
+    save('filteredAbpWithEhr.mat', 'filteredAbpWithEhr')
+    
     %% Run pipeline2
     % Get tensors for ECG and Art Line
-    addpath(genpath('../../BCIL-Shared/tensor_pipeline/'));
+    addpath(genpath('tensor_pipeline/'));
     expName = generateDataAndExperimentName(signalsUsing, gapDuration, signalDuration);
     driver_tensorize(outcomeName + "ECG", "ECG_" + expName + "_TS", "TS", nWindows);
     driver_tensorize(outcomeName + "Art", "Art_" + expName + "_ABP", "ABP", nWindows);
@@ -163,34 +167,4 @@ function runFindQsofaScript(configFile)
                                      iFlowsheet.(iFlowFields{1}));
         save(fullfile(saveLocation, strcat('qsofa', num2str(i))), 'qsofa');
     end
-end
-
-function [wvFeatureTable, featList] = appendEhrData(wvFeatureTable, ehrFeatures)
-% Append EHR data
-
-    ehrBroken = breakCellsIntoColumns(ehrFeatures);
-    [~, uIdx] = unique(ehrBroken(:, {'Sepsis_ID', 'Sepsis_EncID'}));
-    ehrBroken = ehrBroken(uIdx, :);
-    
-    featList = ["Creatinine", "Glucose", "HCT", "Hgb", "INR", "Lactate", ...
-                   "PLT", "Potassium", "Sodium", "WBC", "HR", "MAP", ...
-                   "RespiratoryRate", "SpO2", "Temperature", "FiO2", "PEEP", ...
-                   "Intubated", "UrineOutput", "Dobutamine", "Dopamine", ...
-                   "Epinephrine", "Isoproterenol", "Milrinone", ...
-                   "Norepinephrine", "Vasopressin"];
-   featList = [featList, featList + "Retro"];
-   fidx = ismember(featList, ehrBroken.Properties.VariableNames);
-   featList = featList(fidx);
-   fidx = ismember(ehrBroken.Properties.VariableNames, featList);
-   ehrMat = cell2mat(table2array(ehrBroken(:, fidx)));
-   % Replace unknown value with 0
-   ehrMat(isnan(ehrMat)) = 0;
-   ehrCells = mat2cell(ehrMat, ones(1, size(ehrMat, 1)));
-   ehrTable = table(ehrBroken.Sepsis_ID, ehrBroken.Sepsis_EncID, ehrCells, ...
-                    'VariableNames', ["SepsisID", "Sepsis_EncID", "EHR_Data_Features"]);
-   
-  % This preserves the order (rather than innerjoin)
-   wvFeatureTable = join(wvFeatureTable, ehrTable, ...
-                         'LeftKeys', ["SepsisID", "Sepsis_EncID"], ...
-                         'RightKeys', ["SepsisID", "Sepsis_EncID"]);
 end

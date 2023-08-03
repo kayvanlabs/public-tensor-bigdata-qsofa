a505d3eaff0b0e72dcb1710e6b521230360f8891
Olivia Alge (umich) <oialge@umich.edu>
2023-08-03 13:56:57 -0400

    Remove unused files

--- tensor_pipeline/utils/add_paths.m
+++ tensor_pipeline/utils/add_paths.m
@@ -2,12 +2,10 @@ function add_paths()
     % Add to path functions for feature extraction
     if ispc
         disp("Running on PC");
-        %addpath(genpath('Z:\Projects\DoD_Multimodal\Code\Larry\DOD\matlab'));
-        addpath(genpath('Z:/Projects/Tensor_Sepsis/Code/oialge/tensor-bigdata/DOD/'));
+        addpath(genpath('./'));
     else
         disp("Running on Cluster");
-        %addpath(genpath('/nfs/turbo/med-kayvan-lab/Projects/DoD_Multimodal/Code/Larry/DOD/matlab'));
-        addpath(genpath('/nfs/turbo/med-kayvan-lab/Projects/Tensor_Sepsis/Code/oialge/tensor-bigdata/DOD/'));
+        addpath(genpath('./'));
     end
     disp("Add paths complete");
     disp(" ");
